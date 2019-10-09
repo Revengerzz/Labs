ff002f0845cf2978d947927a3d9c2f8be40d81a8
Aneryd <andrey.zhalo@mail.ru>
2019-10-09 07:34:59 +0000

    Я загрузил лабу

---
+++ pylabs/lab1.py
@@ -0,0 +1,10 @@
+import math
+a = int(input())
+x = int(input())
+print(9*(7*a**2-19*a*x+10*x**2)/(25*a**2+30*a*x+9*x**2))
+a = int(input())
+x = int(input())
+print(math.cos(9*a**2-13*a*x-10*x**2))
+a = int(input())
+x = int(input())
+print(math.log(-80*a**2-46*a*x+21*x**2+1)/math.log(10))
