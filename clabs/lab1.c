4f8acd1bb2a06597f4f279f4ae258453b1ee1a1a
Revengerzz <cfsko@mail.ru>
2019-10-27 19:38:17 +0000

    Изменил лабу

--- clabs/lab1.c
+++ clabs/lab1.c
@@ -1,7 +1,32 @@
+/*comment*/
 #include <stdio.h>
-#include <conio.h>
-main()
+#include <math.h>
+
+int main(void)
 {
-printf('Hello world!');
-getch();
+float a,x,g,f,y;
+int bukva;
+printf("Введите х ");
+scanf("%f", &x);
+printf("Введите a ");
+scanf("%f", &a);
+scanf("%i",&bukva);
+switch(bukva)
+{
+    case 1:
+        g = 9*(7*a*a-19*a*x+10*x*x)/(25*a*a+30*a*x+9*x*x);
+        printf("g=%f\n\n",g);
+        break;
+    case 2:
+        f = cos(9*a*a-13*a*x-10*x*x);
+        printf("f=%f\n\n",f);
+        break;
+    case 3:
+        y = log(-80*a*a-46*a*x+21*x*x+1);
+        printf("y=%f\n\n",y);
+        break;
+    default:
+        printf("неправильный ввод.\n");
+}
+
 }
