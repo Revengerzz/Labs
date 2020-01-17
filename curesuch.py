5449d38bda4b5f7bef6b30d0897d13d0dff25ba5
Revengerzz <Revengerzz@github.com>
2020-01-17 17:07:22 +0300

    sheesh

--- curesuch.py
+++ curesuch.py
@@ -1,13 +1,10 @@
 while True:
     ans = str(input('Хотите начать со вступления?(да/нет): '))
-    print()
     if (ans == 'Да') or (ans == 'да'):
-        print('Тут будет вступление')
+        print('Тут будет вступление\n')
         break
     elif (ans == 'Нет') or (ans == 'нет'):
-        name = str(input('Введи своё имя: '))
-        print()
+        name = str(input('Введи своё имя: \n'))
         break
     else:
         print('Можно писать только "Да" или "Нет" ')
-        print()
